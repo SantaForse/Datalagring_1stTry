ab7236f73c1dcf9afb21f3b9b05fd76e095a1c00
SantaForse <santaforsell@gmail.com>
2023-03-08 22:29:32 +0100

    Add project files.

---
+++ Datalagring_1stTry/Contexts/DataContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Datalagring_1stTry.Models.Entitites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datalagring_1stTry.Contexts;
+
+public partial class DataContext : DbContext
+{
+    public DataContext()
+    {
+    }
+
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Address> Addresses { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\jespe\\Desktop\\Santa_Datalagring\\Datalagring_1stTry\\Datalagring_1stTry\\Contexts\\sql_db.mdf;Integrated Security=True;Connect Timeout=30");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Address>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Addresse__3214EC0787F0CE3F");
+
+            entity.Property(e => e.City).HasMaxLength(100);
+            entity.Property(e => e.PostalCode)
+                .HasMaxLength(6)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.StreetName).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Customer__3214EC070E1379EC");
+
+            entity.HasIndex(e => e.Email, "UQ__Customer__A9D1053415D105C4").IsUnique();
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(13)
+                .IsUnicode(false)
+                .IsFixedLength();
+
+            entity.HasOne(d => d.Address).WithMany(p => p.Customers)
+                .HasForeignKey(d => d.AddressId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Customers__Addre__398D8EEE");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Datalagring_1stTry/Contexts/sql_tables_and_data.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE Addresses (
+	Id int not null identity primary key,
+	StreetName nvarchar(100) not null,
+	PostalCode char (6) not null,
+	City nvarchar (100) not null
+)
+GO
+
+CREATE TABLE Customers (
+	Id uniqueidentifier not null primary key,
+	FirstName nvarchar(50) not null,
+	LastName nvarchar(50) not null,
+	Email nvarchar(100) not null unique,
+	PhoneNumber char(13) null,
+
+	AddressId int not null references Addresses(Id)
+)
+GO
+
+INSERT INTO Addresses VALUES
+	('Rålambsvägen 3', '234 34', 'Stockholm'),
+	('Rålambsvägen 16', '234 32', 'Stockholm'),
+	('Rålambsvägen 78', '234 98', 'Stockholm')
+GO
+
+INSERT INTO Customers VALUES 
+	('b311e882-84f4-4b60-8ce9-597a1307662e', 'Santa', 'Forsell', 'santaforsell@domain.com', '098 876 76 89', 1),
+	('6af9e3de-1b01-4d5c-8768-62f80589db95', 'Luna', 'Forsell', 'jesperforsell@domain.com', '765 987 23 23', 1),
+	('804b070d-dceb-4647-a311-8a9cfc3ac035', 'Sven', 'Svensson', 'svensvernsson@domain.com', '875 435 72 34', 2)
+GO
